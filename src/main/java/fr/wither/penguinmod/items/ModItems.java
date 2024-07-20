188b82bb4df4cbeb75604154c642b5656f2671e6
Alexis Beyrand <wither.plugins@gmail.com>
2024-07-20 18:48:14 +0200

    Update food properties for Pinder Kingui item
    The food properties for the Pinder Kingui item have been updated to provide less nutrition and saturation. For comparison, the saturation and nutrition values have been set closer to that of a golden carrot.

--- src/main/java/fr/wither/penguinmod/items/ModItems.java
+++ src/main/java/fr/wither/penguinmod/items/ModItems.java
@@ -2,6 +2,7 @@ package fr.wither.penguinmod.items;
 
 import fr.wither.penguinmod.utils.Commons;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,9 +13,14 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Commons.MOD_ID);
 
-
+Foods
     public static final RegistryObject<Item> PINDER_KINGUI = ITEMS.register("pinder_kingui",
-            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(15).saturationMod(2.0f).build())));
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+                    // For comparison, golden carrot is nutrition 6 and saturation 1.2f
+                    .nutrition(10)
+                    .saturationMod(1.5f)
+                    .build()
+            )));
 
 
 
